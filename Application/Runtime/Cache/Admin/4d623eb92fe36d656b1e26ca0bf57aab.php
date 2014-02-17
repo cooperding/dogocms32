032a316caeef51c7fd0cf7393020e646288483e7
dingcms <lookcms@gnail.com>
2014-02-17 23:01:02 +0800

    ？？？

--- Application/Runtime/Cache/Admin/4d623eb92fe36d656b1e26ca0bf57aab.php
+++ Application/Runtime/Cache/Admin/4d623eb92fe36d656b1e26ca0bf57aab.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit();?><form action="<?php echo U('NavFoot/update');?>" class="form_dogocms" method="post">
+<?php if (!defined('THINK_PATH')) exit();?><form action="<?php echo U('Admin/NavFoot/update');?>" class="form_dogocms" method="post">
     <input type="hidden" name="id" value="<?php echo ($data["id"]); ?>" />
     <div class="division">
         <table>
@@ -9,22 +9,23 @@
                 </tr>
                 <tr>
                     <th>上级分组名：</th>
-                    <td><input name="parent_id" id="combotree" class="easyui-combotree combotree" data-options="url:'<?php echo U('NavFoot/jsonTree');?>',required:true," value="<?php echo ($data["parent_id"]); ?>"  style="width:200px;"/></td>
+                    <td><input name="parent_id" id="combotree" class="easyui-combotree combotree" data-options="url:'<?php echo U('Admin/NavFoot/jsonTree');?>',required:true," value="<?php echo ($data["parent_id"]); ?>"  style="width:200px;"/></td>
                 </tr>
                 <tr>
                     <th>状态：</th>
-                    <td><html:radio name="status" radios="status" checked="v_status" /></td>
-                </tr>
-                <tr>
-                    <th><?php echo L("keywords");?>：</th>
-                    <td><input type="text" name="keywords" value="<?php echo ($data["keywords"]); ?>" /></td>
-                </tr>
-                <tr>
-                    <th><?php echo L("description");?>：</th><td><textarea name="description" rows="3" cols="30"><?php echo ($data["description"]); ?></textarea><span class="red"></span></td>
+                    <td><input type="radio" checked="checked" name="status[]" value="20">启用<input type="radio" name="status[]" value="10">禁用</td>
+            </tr>
+            <tr>
+                <th><?php echo L("keywords");?>：</th>
+                <td><input type="text" name="keywords" value="<?php echo ($data["keywords"]); ?>" /></td>
+            </tr>
+            <tr>
+                <th><?php echo L("description");?>：</th><td><textarea name="description" rows="3" cols="30"><?php echo ($data["description"]); ?></textarea><span class="red"></span></td>
                 </tr>
                 <tr>
                     <th><?php echo L("orderby");?>：</th><td><input type="text" name="myorder" value="<?php echo ($data["myorder"]); ?>" /></td>
                 </tr>
             </tbody>
-        </table></div>
+        </table>
+    </div>
 </form>
