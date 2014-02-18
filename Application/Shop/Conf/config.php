c62178433767038c1c5747a8068d1403e403757f
dingcms <lookcms@gnail.com>
2014-02-18 22:00:33 +0800

    升级home模板。。。1

---
+++ Application/Shop/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+//    'DEFAULT_C_LAYER'=>'Action',
+    'URL_MODEL' => '2', //URL模式
+    'VIEW_PATH' => './Theme/',
+    'DEFAULT_THEME' => 'default', // 设置默认的模板主题
+    'TMPL_FILE_DEPR' => 'tpl_', //配置简化模板的目录层次
+    //'LAYOUT_ON' => true,
+    'LAYOUT_NAME' => 'layout',
+);
